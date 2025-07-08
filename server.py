34dceead89b4554da837bad1df1e60200fb37df9
Mohammed Salim <renablehots@gmail.com>
2025-07-08 11:37:28 +0400

    Basic jinja usage added

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
 import threading
 
+from jinja2 import Template, Environment, FileSystemLoader
 from flask import Flask
 
 app = Flask(__name__)
+env = Environment(loader=FileSystemLoader("./templates"))
 
 @app.route("/")
 def homepage():
-    return "Hello World!"
+    template = env.get_template("homepage.html")
+    data = {"names": ["John", "Eli", "Michael"]}
+    return template.render(data)
 
 def run_server():
     app.run("0.0.0.0", 60002)
