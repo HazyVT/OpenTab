73093f63299761c7e8ba58c4120f379d05ba5152
Mohammed Salim <renablehots@gmail.com>
2025-07-09 07:17:08 +0400

    htmx folder tree structure

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import os
 import traceback
 
 from jinja2 import Environment, FileSystemLoader
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__, static_url_path="/static")
@@ -24,12 +24,19 @@ def get_extension():
 
 @app.get("/tab-data")
 def get_tab_data():
+    id_param = request.args.get("id")
+
     with open("data.json", "r") as file:
         data = json.load(file)
-        template = env.get_template("tab_data.html")
-        return template.render(data)
-    
-
+        if id_param is None:
+            root_items = [v for v in data["tree"] if v["parent"] == "."]
+            template = env.get_template("tab_data.html")
+            return template.render({"tree": root_items})
+        else:
+            nested_items = [v for v in data["tree"] if v["parent"] == int(id_param)]
+            template = env.get_template("tab_data.html")
+            return template.render({"tree": nested_items})   
+            
 def run_server():
     app.run("0.0.0.0", 60002)
 
